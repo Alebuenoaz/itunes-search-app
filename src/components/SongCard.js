13d5bb7202df029e13b7ffa30409400ae73c1f38
Alejandro Bueno <alebuenoaz@gmail.com>
2021-06-27 14:28:34 -0400

    Improving the Cards functionality, removing unnecesary files and changing presets

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-function SongCard() {
+function SongCard({artistName, imgCover, songName, album, price, releaseDate, audio}) {
 
     return (
         <div className="card text-center bg-dark">
+            {imgCover !== undefined ? <img src={imgCover} alt={songName} /> : <img src="http://hosted.netro.ca/morplay/player/img/blankart.jpg" alt={songName}/>}
             <div className="card-body text-light">
-                <h4 className="card-title">Song Name</h4>
-                <p className="card-text text-secondary">Artist: artistName</p>
-                <p className="card-text text-secondary">Album: album</p>
-                <p className="card-text text-secondary">Release Date: releaseDate</p>
-                <p className="card-text text-secondary">Price: price$</p>
+                <h4 className="card-title">{songName}</h4>
+                <p className="card-text text-secondary">Artist: {artistName}</p>
+                <p className="card-text text-secondary">Album: {album}</p>
+                <p className="card-text text-secondary">Release Date: {releaseDate.split('T')[0]}</p>
+                <p className="card-text text-secondary">Price: {price}$</p>
                 <a href="#!" className="btn btn-outline-secondary rounded-0">Play this Track</a>
             </div>
         </div>
